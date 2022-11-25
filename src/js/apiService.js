420093e4f83cae1a7bdf3c82c7180bb6db848876
Denys Borysiuk <denisborisiuk@gmail.com>
2022-11-25 20:21:33 +0200

    add components from hw task

---
+++ src/js/apiService.js
@@ -0,0 +1,12 @@
+const API_KEY = '31604324-7a50cb95f9ef385a3991a2501';
+const BASE_URL = 'https://pixabay.com/api/';
+
+// export function fetchCountries(name) {
+//   const url = `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flag,languages`;
+//   return fetch(url).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   });
+// }
