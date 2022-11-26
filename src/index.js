128802225fbe0c7427748bf606907ab0b26bacaa
Denys Borysiuk <denisborisiuk@gmail.com>
2022-11-26 19:11:46 +0200

    add new features

--- src/index.js
+++ src/index.js
@@ -1,41 +1,82 @@
 import './css/styles.css';
-// import axios from 'axios';
-import { getPictures } from './js/apiService.js';
+import { getImages } from './js/apiService';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const form = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
 
 form.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
   e.preventDefault();
+
+  const searchTerm = e.target.searchQuery.value;
+
   try {
-    getPictures();
+    getImages(searchTerm).then(renderMarkup);
   } catch (error) {
     console.log(error);
   }
-  //   renderMarkup(params);
 }
 
-function renderMarkup(params) {
-  `<div class="photo-card">
-  <img src="" alt="" loading="lazy" />
+function renderMarkup({ data: { hits, totalHits } }) {
+  console.log(hits);
+  gallery.innerHTML = '';
+  if (totalHits === 0) {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  } else {
+    Notify.success(`Hooray! We found ${totalHits} images.`);
+    const markup = hits
+      .map(
+        ({
+          webformatURL,
+          largeImageURL,
+          tags,
+          likes,
+          views,
+          comments,
+          downloads,
+        }) => {
+          return `<a class="gallery-link" href="${largeImageURL}">
+  <img class="gallery-image" src="${webformatURL}" height="200px" alt="${tags}" data-source="${largeImageURL}" loading="lazy" />
   <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-    </p>
-  </div>
-</div>`;
+     <p class="info-item">
+       <b>Likes</b>
+       ${likes}
+     </p>
+     <p class="info-item">
+       <b>Views</b>
+       ${views}
+     </p>
+     <p class="info-item">
+       <b>Comments</b>
+       ${comments}
+     </p>
+     <p class="info-item">
+       <b>Downloads</b>
+       ${downloads}
+     </p>
+   </div>
+   </a>`;
+        }
+      )
+      .join('');
+
+    gallery.insertAdjacentHTML('beforeend', markup);
+    var lightbox = new SimpleLightbox('.gallery a', {
+      /* options */
+      captionSelector: 'img',
+      captionType: 'attr',
+      captionsData: 'alt',
+      captionPosition: 'bottom',
+      captionDelay: 250,
+    });
+  }
 }
 
 // const { height: cardHeight } = document
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -3,21 +3,27 @@ import axios from 'axios';
 const API_KEY = '31604324-7a50cb95f9ef385a3991a2501';
 const BASE_URL = 'https://pixabay.com/api/';
 
-// export function fetchCountries(name) {
-//   const url = `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flag,languages`;
-//   return fetch(url).then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.status);
-//     }
-//     return response.json();
-//   });
-// }
-
-export async function getPictures() {
+export async function getImages(searchTerm) {
   try {
-    const response = await axios.get(`${BASE_URL}`);
-    console.log(response);
+    const response = await axios.get(
+      `${BASE_URL}?key=${API_KEY}&q=${searchTerm}&image_type=photo&orientation=horizontal&safesearch=true`
+    );
+    return response;
   } catch (error) {
     console.error(error);
   }
 }
+
+// export async function getPictures() {
+//   try {
+//     const response = await axios.get({
+//       baseURL: 'https://pixabay.com/api/',
+//       params: {
+//         KEY: '31604324-7a50cb95f9ef385a3991a2501',
+//       },
+//     });
+//     console.log(response);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
